f5c79c3e1f2fdef1a6957e3dc05e68cc955b5be5
yarnb <yarnb@foxmail.com>
2024-06-10 13:08:16 +0800

    chore

--- admin/src/App/Layout/Content/AppList/index.tsx
+++ admin/src/App/Layout/Content/AppList/index.tsx
@@ -1,9 +1,18 @@
+import { Typography } from "antd";
 import ItemList from "./ItemList";
 
 export const AppList = () => {
   return (
     <>
-      <ItemList />
+      <div className='flex justify-center'>
+        <div className="max-w-4xl">
+          <Typography.Title level={2}>应用列表</Typography.Title>
+          <Typography.Paragraph>
+            Below you'll find the list of apps that you have access to
+          </Typography.Paragraph>
+          <ItemList />
+        </div>
+      </div>
     </>
   );
 };
