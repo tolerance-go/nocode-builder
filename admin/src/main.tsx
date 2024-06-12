e37db21f5633fdc398c388529f363892f1d7122d
yarnb <yarnb@foxmail.com>
2024-06-12 23:09:08 +0800

    chore

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import "./i18n"; // 引入 i18n 配置
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
