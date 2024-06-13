f92b88704266f4f5740db8575de30bdfeed4aeea
yarnb <yarnb@foxmail.com>
2024-06-13 12:40:56 +0800

    chore

--- admin/src/App/index.tsx
+++ admin/src/App/index.tsx
@@ -2,8 +2,16 @@ import Route from "@/components/Route";
 import "@antv/s2-react/dist/style.min.css";
 import { Layout } from "./Layout";
 import "./index.css";
+import useNavigate from "@/hooks/useNavigate";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/apps");
+  }, []);
+
   return <Route path="/*" element={<Layout />}></Route>;
 }
 
