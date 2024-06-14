aa193290d22d74f7006297b645515a7ba57d9383
yarnb <yarnb@foxmail.com>
2024-06-14 23:55:53 +0800

    chore

--- admin/vitest.config.ts
+++ admin/vitest.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// 获取当前文件的路径
+const __filename = fileURLToPath(import.meta.url);
+// 获取当前文件所在的目录
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./setupTests.ts",
+    setupFiles: path.resolve(__dirname, "./setupTests.ts"), // 使用绝对路径
   },
   resolve: {
     alias: {
-      "@": path.resolve("src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
