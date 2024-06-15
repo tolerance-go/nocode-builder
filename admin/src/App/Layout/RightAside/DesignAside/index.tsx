3cc2c0cf23ca08989eec7d7dcd106db490f173c6
yarnb <yarnb@foxmail.com>
2024-06-15 21:18:38 +0800

    chore

--- admin/src/App/Layout/RightAside/DesignAside/index.tsx
+++ admin/src/App/Layout/RightAside/DesignAside/index.tsx
@@ -2,6 +2,8 @@ import { DesignAsideType } from "@/types";
 import { useSearchParams } from "react-router-dom";
 import ComponentSettingsForm from "./ComponentSettingsForm";
 import { ComponentStorePanel } from "./ComponentStorePanel";
+import { css } from "@emotion/css";
+import { Tabs, Space, Button } from "antd";
 
 export const DesignAside = () => {
   const [searchParams] = useSearchParams({
@@ -13,6 +15,45 @@ export const DesignAside = () => {
   }
 
   if (searchParams.get("designAsideType") === "settings") {
-    return <ComponentSettingsForm />;
+    return (
+      <div>
+        <Tabs
+          tabBarExtraContent={
+            <Space className="px-2">
+              <Button type="text" size="small">
+                添加
+              </Button>
+            </Space>
+          }
+          className={css`
+            .ant-tabs-nav {
+              margin-bottom: 0;
+              height: 34px;
+            }
+            .ant-tabs-nav .ant-tabs-tab {
+              border-top: 0;
+              border-left: 0;
+            }
+          `}
+          type="card"
+          size={"small"}
+          items={[
+            {
+              label: "配置",
+              key: "setting",
+            },
+            {
+              label: "样式",
+              key: "style",
+            },
+            {
+              label: "蓝图",
+              key: "bluemap",
+            },
+          ]}
+        />
+        <ComponentSettingsForm />
+      </div>
+    );
   }
 };
