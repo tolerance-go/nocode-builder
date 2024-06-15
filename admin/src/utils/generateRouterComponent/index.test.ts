d72eef53dc5de7cbd989c31cf194ed3476e55271
yarnb <yarnb@foxmail.com>
2024-06-16 01:16:35 +0800

    chore

--- admin/src/utils/generateRouterComponent/index.test.ts
+++ admin/src/utils/generateRouterComponent/index.test.ts
@@ -25,28 +25,22 @@ describe("generateRouterComponent", () => {
         ],
       },
     ];
-    expect(generateRouterComponent(nodeDatas)).toEqual({
-      type: "Router",
-      children: {
-        type: "Routes",
-        children: [
+    expect(generateRouterComponent(nodeDatas)).toEqual([
+      {
+        type: "Route",
+        path: "/",
+        element: [
           {
-            type: "Route",
-            path: "/",
-            element: [
-              {
-                id: "1-1",
-                elementType: "div",
-                staticProps: {},
-                fromWidgetId: "",
-                settings: {},
-                children: [],
-              },
-            ],
+            id: "1-1",
+            elementType: "div",
+            staticProps: {},
+            fromWidgetId: "",
+            settings: {},
+            children: [],
           },
         ],
       },
-    });
+    ]);
   });
 
   it("test2", () => {
@@ -142,108 +136,102 @@ describe("generateRouterComponent", () => {
       },
     ];
 
-    expect(generateRouterComponent(nodeDatas)).toEqual({
-      type: "Router",
-      children: {
-        type: "Routes",
-        children: [
+    expect(generateRouterComponent(nodeDatas)).toEqual([
+      {
+        type: "Route",
+        path: "/",
+        element: [
           {
-            type: "Route",
-            path: "/",
-            element: [
+            id: "1",
+            elementType: "Route",
+            staticProps: {},
+            settings: {
+              path: "/",
+            },
+            fromWidgetId: "",
+            children: [
               {
-                id: "1",
-                elementType: "Route",
+                id: "1-1",
+                elementType: "Layout",
                 staticProps: {},
-                settings: {
-                  path: "/",
-                },
                 fromWidgetId: "",
-                children: [
-                  {
-                    id: "1-1",
-                    elementType: "Layout",
-                    staticProps: {},
-                    fromWidgetId: "",
-                    settings: {},
-                    children: {
-                      header: [
+                settings: {},
+                children: {
+                  header: [
+                    {
+                      id: "1-1-1",
+                      elementType: "Header",
+                      staticProps: {},
+                      fromWidgetId: "",
+                      settings: {},
+                      children: [],
+                    },
+                  ],
+                  sidebar: [
+                    {
+                      id: "1-1-2",
+                      elementType: "Sidebar",
+                      staticProps: {},
+                      fromWidgetId: "",
+                      settings: {},
+                      children: [],
+                    },
+                  ],
+                  content: [
+                    {
+                      id: "1-1-3",
+                      elementType: "Content",
+                      staticProps: {},
+                      fromWidgetId: "",
+                      settings: {},
+                      children: [
                         {
-                          id: "1-1-1",
-                          elementType: "Header",
+                          id: "1-1-3_outlet",
+                          elementType: "Outlet",
                           staticProps: {},
                           fromWidgetId: "",
                           settings: {},
                           children: [],
-                        },
-                      ],
-                      sidebar: [
-                        {
-                          id: "1-1-2",
-                          elementType: "Sidebar",
-                          staticProps: {},
-                          fromWidgetId: "",
-                          settings: {},
-                          children: [],
-                        },
-                      ],
-                      content: [
-                        {
-                          id: "1-1-3",
-                          elementType: "Content",
-                          staticProps: {},
-                          fromWidgetId: "",
-                          settings: {},
-                          children: [
-                            {
-                              id: "1-1-3_outlet",
-                              elementType: "Outlet",
-                              staticProps: {},
-                              fromWidgetId: "",
-                              settings: {},
-                              children: [],
-                            },
-                          ],
                         },
                       ],
                     },
-                  },
-                ],
+                  ],
+                },
               },
             ],
-            children: [
+          },
+        ],
+        children: [
+          {
+            type: "Route",
+            path: "route1",
+            element: [
               {
-                type: "Route",
-                path: "route1",
-                element: [
-                  {
-                    id: "1-1-3-1-1",
-                    elementType: "div",
-                    staticProps: {},
-                    fromWidgetId: "",
-                    settings: {},
-                    children: [],
-                  },
-                ],
+                id: "1-1-3-1-1",
+                elementType: "div",
+                staticProps: {},
+                fromWidgetId: "",
+                settings: {},
+                children: [],
               },
+            ],
+          },
+          {
+            type: "Route",
+            path: "route2",
+            element: [
               {
-                type: "Route",
-                path: "route2",
-                element: [
-                  {
-                    id: "1-1-3-2-1",
-                    elementType: "div",
-                    staticProps: {},
-                    fromWidgetId: "",
-                    settings: {},
-                    children: [],
-                  },
-                ],
+                id: "1-1-3-2-1",
+                elementType: "div",
+                staticProps: {},
+                fromWidgetId: "",
+                settings: {},
+                children: [],
               },
             ],
           },
         ],
       },
-    });
+    ]);
   });
 });
