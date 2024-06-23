8dabfafe69d250c2455e4a8849a562e224780553
yarnb <yarnb@foxmail.com>
2024-06-24 06:06:25 +0800

    chore

--- admin/src/App/Layout/Content/AppContent/AppTemplate/CardList.tsx
+++ admin/src/App/Layout/Content/AppContent/AppTemplate/CardList.tsx
@@ -35,8 +35,8 @@ export const CardList: React.FC<CardListProps> = ({ templates }) => (
                 height: template.type === "desktop" ? 200 : 400,
               }}
             />
-            <div className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-200">
-              <div className="w-full flex flex-col gap-2 items-center justify-center bg-gray-900 bg-opacity-50 hover:bg-opacity-85 transition-all">
+            <div className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-100">
+              <div className="w-full flex flex-col gap-2 items-center justify-center bg-gray-900 bg-opacity-50 hover:bg-opacity-75 transition-all">
                 <Button ghost icon={<EyeOutlined />}>
                   预览
                 </Button>
