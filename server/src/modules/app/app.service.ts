e6e53c5f80e38a8ff44ebe897502f7ab65ad8929
yarnb <yarnb@foxmail.com>
2024-06-25 15:12:55 +0800

    chore

--- server/src/modules/app/app.service.ts
+++ server/src/modules/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 import { App, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
