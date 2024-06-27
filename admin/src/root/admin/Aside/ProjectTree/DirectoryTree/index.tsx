bc83d434783fd5b6904eeb82a471edb74de24e94
yarnb <yarnb@foxmail.com>
2024-06-27 20:05:06 +0800

    chore

--- admin/src/root/admin/Aside/ProjectTree/DirectoryTree/index.tsx
+++ admin/src/root/admin/Aside/ProjectTree/DirectoryTree/index.tsx
@@ -2,7 +2,7 @@ import { useKeyPress } from "@/hooks/useKeyPress";
 import { projectTreeStore } from "@/stores";
 import { ProjectTreeDataNode } from "@/types/tree";
 import { css } from "@emotion/css";
-import { Tree } from "antd";
+import { Tree, Typography } from "antd";
 import React from "react";
 import { useSnapshot } from "valtio";
 import { Title } from "./Title";
@@ -13,6 +13,8 @@ export const TreeMenu = () => {
   const { treeData, expandedKeys, containerHeight } = useSnapshot(
     projectTreeStore.projectTreeState,
   );
+
+  console.log("containerHeight", containerHeight);
 
   useKeyPress(["Delete", "Backspace"], () => {
     projectTreeStore.projectTreeState.selectedKey &&
@@ -29,31 +31,34 @@ export const TreeMenu = () => {
   });
 
   return (
-    <div>
-      <DirectoryTree
-        height={containerHeight}
-        className={css`
-          .ant-tree-node-content-wrapper {
-            display: inline-flex;
-            .ant-tree-title {
-              flex-grow: 1;
-            }
+    <DirectoryTree
+      height={containerHeight}
+      virtual
+      className={css`
+        .ant-tree-node-content-wrapper {
+          display: inline-flex;
+          .ant-tree-title {
+            flex-grow: 1;
           }
-        `}
-        expandedKeys={expandedKeys as React.Key[]} // 受控展开状态
-        onSelect={(keys) => {
-          projectTreeStore.setSelectedKeyAction(
-            (keys.length > 0 ? keys[0] : null) as string | null,
-          );
-        }}
-        onExpand={(keys) => {
-          projectTreeStore.setExpandedKeysAction(keys as string[]); // 更新展开状态
-        }}
-        treeData={treeData as ProjectTreeDataNode[]}
-        titleRender={(nodeData) => (
-          <Title nodeKey={nodeData.key} title={nodeData.title} isEditing={nodeData.isEditing} />
-        )}
-      />
-    </div>
+        }
+      `}
+      expandedKeys={expandedKeys as React.Key[]} // 受控展开状态
+      onSelect={(keys) => {
+        projectTreeStore.setSelectedKeyAction(
+          (keys.length > 0 ? keys[0] : null) as string | null,
+        );
+      }}
+      onExpand={(keys) => {
+        projectTreeStore.setExpandedKeysAction(keys as string[]); // 更新展开状态
+      }}
+      treeData={treeData as ProjectTreeDataNode[]}
+      titleRender={(nodeData) => (
+        <Title
+          nodeKey={nodeData.key}
+          title={nodeData.title}
+          isEditing={nodeData.isEditing}
+        />
+      )}
+    />
   );
 };
