2d85948a781835e0441e92f7420c348ba3bd4977
yarnb <yarnb@foxmail.com>
2024-06-28 01:03:59 +0800

    chore

--- admin/src/root/test/index.tsx
+++ admin/src/root/test/index.tsx
@@ -1,54 +1,54 @@
-import {
-  addNodeAction,
-  removeNodeAction,
-  treeMapStore,
-  treeStore,
-  updateNodeAction,
-} from "@/stores/db/stores";
-import { Button, Flex, Space, Typography } from "antd";
-import { useSnapshot } from "valtio";
+// import {
+//   addNodeAction,
+//   removeNodeAction,
+//   treeMapStore,
+//   treeStore,
+//   updateNodeAction,
+// } from "@/stores/db/stores";
+// import { Button, Flex, Space, Typography } from "antd";
+// import { useSnapshot } from "valtio";
 
-export const Test = () => {
-  const { data } = useSnapshot(treeStore);
-  const { data: treeMapStoreData } = useSnapshot(treeMapStore);
-  return (
-    <>
-      <Flex>
-        <Space>
-          <Button
-            onClick={() => {
-              updateNodeAction(4, "new");
-            }}
-          >
-            更新
-          </Button>
-          <Button
-            onClick={() => {
-              addNodeAction(4, {
-                name: "new",
-                id: 100,
-              });
-            }}
-          >
-            插入
-          </Button>
-          <Button
-            onClick={() => {
-              removeNodeAction(4);
-            }}
-          >
-            删除
-          </Button>
-        </Space>
-      </Flex>
-      <Typography.Paragraph>
-        <Flex gap={4}>
-          <pre style={{ width: "50%" }}>{JSON.stringify(data, null, 2)}</pre>
-          <pre style={{ width: "50%" }}>
-            {JSON.stringify(Array.from(treeMapStoreData), null, 2)}
-          </pre>
-        </Flex>
-      </Typography.Paragraph>
-    </>
-  );
-};
+// export const Test = () => {
+//   const { data } = useSnapshot(treeStore);
+//   const { data: treeMapStoreData } = useSnapshot(treeMapStore);
+//   return (
+//     <>
+//       <Flex>
+//         <Space>
+//           <Button
+//             onClick={() => {
+//               updateNodeAction(4, "new");
+//             }}
+//           >
+//             更新
+//           </Button>
+//           <Button
+//             onClick={() => {
+//               addNodeAction(4, {
+//                 name: "new",
+//                 id: 100,
+//               });
+//             }}
+//           >
+//             插入
+//           </Button>
+//           <Button
+//             onClick={() => {
+//               removeNodeAction(4);
+//             }}
+//           >
+//             删除
+//           </Button>
+//         </Space>
+//       </Flex>
+//       <Typography.Paragraph>
+//         <Flex gap={4}>
+//           <pre style={{ width: "50%" }}>{JSON.stringify(data, null, 2)}</pre>
+//           <pre style={{ width: "50%" }}>
+//             {JSON.stringify(Array.from(treeMapStoreData), null, 2)}
+//           </pre>
+//         </Flex>
+//       </Typography.Paragraph>
+//     </>
+//   );
+// };
