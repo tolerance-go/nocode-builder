99f8a200ee860482e475781c09aa3d8620ebe541
yarnb <yarnb@foxmail.com>
2024-06-28 23:59:10 +0800

    chore

--- admin/src/root/index.tsx
+++ admin/src/root/index.tsx
@@ -1,15 +1,12 @@
 import { useAppStore } from "@/store";
 import { locationState } from "@/stores/route";
-import "@antv/s2-react/dist/style.min.css";
 import { ConfigProvider, theme } from "antd";
 import { useLayoutEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import "@antv/s2-react/dist/style.min.css";
 
 export function Root() {
   const location = useLocation();
-  const firstName = useAppStore.use.firstName();
-  const updateFirstName = useAppStore.use.updateFirstName();
-  const lastName = useAppStore.use.lastName();
   const updatePathname = useAppStore.use.updatePathname();
 
   useLayoutEffect(() => {
@@ -23,14 +20,6 @@ export function Root() {
         algorithm: [theme.darkAlgorithm],
       }}
     >
-      <div
-        onClick={() => {
-          updateFirstName("yarnb");
-        }}
-      >
-        {firstName}
-        {lastName}
-      </div>
       <Outlet />
     </ConfigProvider>
   );
