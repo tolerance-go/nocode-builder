06f6714d79003f668991f09699ab25ac203bc533
yarnb <yarnb@foxmail.com>
2024-06-29 07:02:32 +0800

    chore

--- admin/src/subscribes/index.ts
+++ admin/src/subscribes/index.ts
@@ -2,18 +2,7 @@ import { useAppStoreBase } from "@/store";
 import { isAuthRelatedPath, isSystemPath } from "@/utils";
 import { buildProjectStructureTreeDataMeta } from "./_utils/buildProjectStructureTreeDataMeta";
 
-const firstNameSubscriber = (
-  ...[state, previous]: Parameters<
-    Parameters<typeof useAppStoreBase.subscribe>[0]
-  >
-) => {
-  if (state.firstName !== previous.firstName) {
-    useAppStoreBase.getState().updateLastName(state.firstName);
-  }
-};
-
 useAppStoreBase.subscribe((state, previous) => {
-  firstNameSubscriber(state, previous);
   const {
     initProjectTreeDataMeta,
     loadProjectGroupTableData,
