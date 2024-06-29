aa24f388c59a2cadc2914af6fe51b6a4b7c55f45
yarnb <yarnb@foxmail.com>
2024-06-30 02:40:16 +0800

    chore

---
+++ admin/src/db/index.ts
@@ -0,0 +1,17 @@
+import Dexie, { Table } from "dexie";
+import { Item } from "../models/Item";
+
+export class Database extends Dexie {
+  items: Table<Item, number>;
+
+  constructor() {
+    super("Database");
+    this.version(1).stores({
+      items: "++id, name, value",
+    });
+
+    this.items = this.table("items");
+  }
+}
+
+export const db = new Database();
