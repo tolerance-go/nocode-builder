b848cf6418ea9bc53db9b71ca8163888fb71b280
yarnb <yarnb@foxmail.com>
2024-06-29 08:22:38 +0800

    chore

--- admin/src/store/createServerSlice.ts
+++ admin/src/store/createServerSlice.ts
@@ -5,17 +5,22 @@ export type ServerStates = {
   dataBaseTimelineChunks: DataBaseTimelineChunk[];
   currentTimelineItems: DataBaseTimelineItem[];
   hasSetTimelinePoolCheckInterval: boolean;
+  currentChunksIndex: number;
 };
 
 export type ServerActions = {
   addTimelineItemToPool: (item: DataBaseTimelineItem) => void;
   createTimelineChunkFromPool: () => void;
-  updateSyncedLocal: (chunkIndex: number, syncedLocalAt: string) => void;
-  updateSyncedRemote: (chunkIndex: number, syncedRemoteAt: string) => void;
+  updateSyncedLocal: (chunkIndex: number) => void;
+  updateSyncedRemote: (chunkIndex: number) => void;
   setTimelinePoolCheckInterval: () => void;
+  consumeChunks: () => void;
+  syncChunkToLocal: (chunk: DataBaseTimelineChunk) => void;
 };
 
 export type ServerSlice = ServerStates & ServerActions;
+
+const syncChunkToRemote = async (chunk: DataBaseTimelineChunk) => {};
 
 export const createServerSlice: ImmerStateCreator<ServerSlice, ServerSlice> = (
   set,
@@ -24,6 +29,50 @@ export const createServerSlice: ImmerStateCreator<ServerSlice, ServerSlice> = (
   dataBaseTimelineChunks: [],
   currentTimelineItems: [],
   hasSetTimelinePoolCheckInterval: false,
+  currentChunksIndex: -1,
+
+  syncChunkToLocal: (chunk) => {
+    console.log(chunk);
+  },
+
+  consumeChunks: async () => {
+    const state = get();
+
+    for (
+      let i = state.currentChunksIndex + 1;
+      i < state.dataBaseTimelineChunks.length;
+      i++
+    ) {
+      const chunk = state.dataBaseTimelineChunks[i];
+
+      if (!chunk.hasSyncedLocal) {
+        try {
+          // 同步本地
+          state.syncChunkToLocal(chunk); // 假设someLocalSyncMethod是同步本地的其他slice的方法
+
+          state.updateSyncedLocal(i);
+        } catch {
+          break;
+        }
+      }
+
+      if (!chunk.hasSyncedRemote) {
+        try {
+          // 调用接口同步远程
+          await syncChunkToRemote(chunk); // 假设syncRemote是一个返回同步结果的接口
+
+          state.updateSyncedRemote(i);
+        } catch {
+          // 如果远程同步失败，则跳出循环，稍后重试
+          break;
+        }
+      }
+
+      set((state) => {
+        state.currentChunksIndex = i;
+      });
+    }
+  },
 
   addTimelineItemToPool: (item: DataBaseTimelineItem) =>
     set((state) => {
@@ -49,27 +98,32 @@ export const createServerSlice: ImmerStateCreator<ServerSlice, ServerSlice> = (
     );
   },
 
-  updateSyncedLocal: (chunkIndex: number, syncedLocalAt: string) =>
+  updateSyncedLocal: (chunkIndex: number) =>
     set((state) => {
       if (state.dataBaseTimelineChunks[chunkIndex]) {
         state.dataBaseTimelineChunks[chunkIndex].hasSyncedLocal = true;
-        state.dataBaseTimelineChunks[chunkIndex].syncedLocalAt = syncedLocalAt;
+        state.dataBaseTimelineChunks[chunkIndex].syncedLocalAt =
+          new Date().toISOString();
       }
     }),
 
-  updateSyncedRemote: (chunkIndex: number, syncedRemoteAt: string) =>
+  updateSyncedRemote: (chunkIndex: number) =>
     set((state) => {
       if (state.dataBaseTimelineChunks[chunkIndex]) {
         state.dataBaseTimelineChunks[chunkIndex].hasSyncedRemote = true;
         state.dataBaseTimelineChunks[chunkIndex].syncedRemoteAt =
-          syncedRemoteAt;
+          new Date().toISOString();
       }
     }),
 
   setTimelinePoolCheckInterval: () => {
     setInterval(() => {
-      if (get().currentTimelineItems.length > 0) {
-        get().createTimelineChunkFromPool();
+      const state = get();
+      if (state.currentTimelineItems.length > 0) {
+        state.createTimelineChunkFromPool();
+      }
+      if (state.dataBaseTimelineChunks.length > 0) {
+        state.consumeChunks();
       }
     }, 1000);
     set((state) => {
