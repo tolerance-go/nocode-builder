5d661c2d995c9e45e0964bcd197e916403f100d6
yarnb <yarnb@foxmail.com>
2024-07-01 00:22:52 +0800

    chore

--- admin/src/store/index.ts
+++ admin/src/store/index.ts
@@ -1,6 +1,6 @@
 import { createSelectors } from '@/utils';
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { LayoutSlice, createLayoutSlice } from './createLayoutSlice';
 import { LocationSlice, createLocationSlice } from './createLocationSlice';
@@ -16,15 +16,20 @@ export const useAppStoreBase = create<
   ProjectTreeSlice & NetworkSlice & LocationSlice & LayoutSlice & ServerSlice
 >()(
   devtools(
-    immer((...a) => {
-      return {
-        ...createServerSlice(...a),
-        ...createLayoutSlice(...a),
-        ...createLocationSlice(...a),
-        ...createNetworkSlice(...a),
-        ...createProjectTreeSlice(...a),
-      };
-    }),
+    persist(
+      immer((...a) => {
+        return {
+          ...createServerSlice(...a),
+          ...createLayoutSlice(...a),
+          ...createLocationSlice(...a),
+          ...createNetworkSlice(...a),
+          ...createProjectTreeSlice(...a),
+        };
+      }),
+      {
+        name: 'localStore',
+      },
+    ),
   ),
 );
 
