e231ac767053dc524091db5f38cce9be3d947842
yarnb <yarnb@foxmail.com>
2024-07-20 00:38:50 +0800

    chore

--- scripts/start-with-version.js
+++ scripts/start-with-version.js
@@ -1,4 +1,6 @@
 import { getVersion, executeCommand } from './utils.js';
+const remoteRegistry = 'registry.cn-heyuan.aliyuncs.com';
+const namespace = 'unocode';
 
 // 获取版本号
 const version = await getVersion();
@@ -6,6 +8,7 @@ const version = await getVersion();
 // 设置环境变量并运行 docker-compose up -d
 executeCommand('cross-env', [
   `APP_TAG=${version}`,
+  `REGISTRY_PATH=${remoteRegistry}/${namespace}`,
   'docker-compose',
   'up',
   '-d',
