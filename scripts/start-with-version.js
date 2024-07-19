425ca6dae738499c0678d1bb435ba34c62bbf53e
yarnb <yarnb@foxmail.com>
2024-07-20 02:31:05 +0800

    chore

--- scripts/start-with-version.js
+++ scripts/start-with-version.js
@@ -5,10 +5,12 @@ import { remoteRegistry, namespace } from './config.js';
 const version = await getVersion();
 
 // 设置环境变量并运行 docker-compose up -d
-executeCommand('cross-env', [
+await executeCommand('cross-env', [
   `APP_TAG=${version}`,
   `REGISTRY_PATH=${remoteRegistry}/${namespace}`,
   'docker-compose',
+  '-f',
+  'docker-compose.prod.yml',
   'up',
   '-d',
 ]);
