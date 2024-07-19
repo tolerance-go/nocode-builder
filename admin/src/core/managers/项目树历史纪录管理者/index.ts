ce17f36233b9bd30e61c30b016f58cacb5659b7c
yarnb <yarnb@foxmail.com>
2024-07-20 03:43:57 +0800

    chore

--- admin/src/core/managers/项目树历史纪录管理者/index.ts
+++ admin/src/core/managers/项目树历史纪录管理者/index.ts
@@ -55,7 +55,10 @@ export class 项目树历史纪录管理者 extends ManagerBase {
     this.引擎api = 引擎api;
 
     this.历史状态机Actor = createActor(历史状态机, {
-      inspect: window.Cypress ? undefined : createBrowserInspector().inspect,
+      inspect:
+        window.Cypress || import.meta.env.PROD
+          ? undefined
+          : createBrowserInspector().inspect,
       input: {
         历史堆栈: this.历史堆栈,
         历史指针: this.历史指针,
