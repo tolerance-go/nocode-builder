2335fd4589153b11f5af946e1929ee709c2e96fd
yarnb <yarnb@foxmail.com>
2024-07-22 00:26:08 +0800

    chore

--- admin/src/engines/BaseEngine.ts
+++ admin/src/engines/BaseEngine.ts
@@ -1,16 +1,16 @@
 import { EngineBase } from '@/base';
-import { AsyncStateManager } from '@/modules/managers';
+import { AsyncStateManager } from '@/modules/AsyncStateManager';
 
 export class BaseEngine extends EngineBase {
   providerModules() {
     super.providerModules(new AsyncStateManager());
   }
 
-  protected async onLaunch(): Promise<void> {
-    const asyncStateManager = this.getModule(AsyncStateManager);
-  }
-
   updateState() {}
 
   getState() {}
+
+  protected async onLaunch(): Promise<void> {
+    const asyncStateManager = this.getModule(AsyncStateManager);
+  }
 }
