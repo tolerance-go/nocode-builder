dd283019d95342615259d8abf5d3252554417119
yarnb <yarnb@foxmail.com>
2024-08-03 12:55:26 +0800

    chore

--- admin/src/modules/界面组件树管理模块/root/(admin)/Layout/内容区域组件/view-editor/WidgetDrawer/CardItem.tsx
+++ admin/src/modules/界面组件树管理模块/root/(admin)/Layout/内容区域组件/view-editor/WidgetDrawer/CardItem.tsx
@@ -1,5 +1,6 @@
 import { FileImageOutlined } from '@ant-design/icons';
 import { Card, Typography, theme } from 'antd';
+import { useDrag } from 'react-dnd';
 
 // 定义数据类型
 export type CardData = {
@@ -10,41 +11,50 @@ export type CardData = {
 
 export const CardItem = ({ item }: { item: CardData }) => {
   const { token } = theme.useToken();
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'CARD',
+    item: { title: item.title },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
 
   return (
-    <Card
-      size="small"
-      hoverable
-      bordered={false}
-      cover={
-        item.image ? (
-          <img
-            style={{
-              width: 200,
-              height: 150,
-              objectFit: 'cover',
-            }}
-            alt={item.title}
-            src={item.image}
-          />
-        ) : (
-          <div
-            style={{
-              width: '100%',
-              height: 150,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: '4px',
-              backgroundColor: token.colorBgBase,
-            }}
-          >
-            <FileImageOutlined />
-          </div>
-        )
-      }
-    >
-      <Typography.Text strong>{item.title}</Typography.Text>
-    </Card>
+    <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
+      <Card
+        size="small"
+        hoverable
+        bordered={false}
+        cover={
+          item.image ? (
+            <img
+              style={{
+                width: 200,
+                height: 150,
+                objectFit: 'cover',
+              }}
+              alt={item.title}
+              src={item.image}
+            />
+          ) : (
+            <div
+              style={{
+                width: '100%',
+                height: 150,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: '4px',
+                backgroundColor: token.colorBgBase,
+              }}
+            >
+              <FileImageOutlined />
+            </div>
+          )
+        }
+      >
+        <Typography.Text strong>{item.title}</Typography.Text>
+      </Card>
+    </div>
   );
 };
--- admin/src/modules/界面组件树管理模块/root/(admin)/Layout/内容区域组件/view-editor/index.tsx
+++ admin/src/modules/界面组件树管理模块/root/(admin)/Layout/内容区域组件/view-editor/index.tsx
@@ -4,6 +4,8 @@ import { Button, theme } from 'antd';
 import { WidgetTree } from './WidgetTree';
 import { WidgetDrawer, WidgetDrawerRef } from './WidgetDrawer';
 import { useRef } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export const ViewEditor = () => {
   const { token } = theme.useToken();
@@ -11,108 +13,112 @@ export const ViewEditor = () => {
   const widgetOpenBtnRef = useRef<HTMLButtonElement>(null);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        width: '100%',
-      }}
-      data-test-id={组件测试标识.视图编辑页面.区域}
-    >
+    <DndProvider backend={HTML5Backend}>
       <div
         style={{
           display: 'flex',
-          flexGrow: 1,
-          position: 'relative',
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%',
         }}
+        data-test-id={组件测试标识.视图编辑页面.区域}
       >
-        <div
-          style={{
-            backgroundColor: token.colorBgContainer,
-            borderRight: `1px solid ${token.colorBorderSecondary}`,
-            width: 300,
-          }}
-        >
-          <WidgetTree />
-        </div>
         <div
           style={{
             display: 'flex',
             flexGrow: 1,
+            position: 'relative',
           }}
         >
           <div
             style={{
-              flexGrow: 1,
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-            data-test-id={组件测试标识.视图编辑页面.舞台.区域}
-          >
-            <div
-              style={{
-                borderBottom: `1px solid ${token.colorBorderSecondary}`,
-                backgroundColor: token.colorBgContainer,
-                padding: layoutPadding(token),
-              }}
-              data-test-id={组件测试标识.视图编辑页面.舞台.头部}
-            >
-              header
-            </div>
-            <div data-test-id={组件测试标识.视图编辑页面.舞台.内容}>stage</div>
-          </div>
-          <div
-            style={{
-              width: 300,
-              borderLeft: `1px solid ${token.colorBorderSecondary}`,
               backgroundColor: token.colorBgContainer,
-              display: 'flex',
-              flexDirection: 'column',
+              borderRight: `1px solid ${token.colorBorderSecondary}`,
+              width: 300,
             }}
-            data-test-id={组件测试标识.视图编辑页面.配置栏目.区域}
+          >
+            <WidgetTree />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexGrow: 1,
+            }}
           >
             <div
               style={{
                 flexGrow: 1,
+                display: 'flex',
+                flexDirection: 'column',
               }}
-              data-test-id={组件测试标识.视图编辑页面.配置栏目.上部}
+              data-test-id={组件测试标识.视图编辑页面.舞台.区域}
             >
-              toolPanel-top
+              <div
+                style={{
+                  borderBottom: `1px solid ${token.colorBorderSecondary}`,
+                  backgroundColor: token.colorBgContainer,
+                  padding: layoutPadding(token),
+                }}
+                data-test-id={组件测试标识.视图编辑页面.舞台.头部}
+              >
+                header
+              </div>
+              <div data-test-id={组件测试标识.视图编辑页面.舞台.内容}>
+                stage
+              </div>
             </div>
             <div
               style={{
-                flexGrow: 1,
-                borderTop: `1px solid ${token.colorBorderSecondary}`,
+                width: 300,
+                borderLeft: `1px solid ${token.colorBorderSecondary}`,
+                backgroundColor: token.colorBgContainer,
+                display: 'flex',
+                flexDirection: 'column',
               }}
-              data-test-id={组件测试标识.视图编辑页面.配置栏目.下部}
+              data-test-id={组件测试标识.视图编辑页面.配置栏目.区域}
             >
-              toolPanel-bottom
+              <div
+                style={{
+                  flexGrow: 1,
+                }}
+                data-test-id={组件测试标识.视图编辑页面.配置栏目.上部}
+              >
+                toolPanel-top
+              </div>
+              <div
+                style={{
+                  flexGrow: 1,
+                  borderTop: `1px solid ${token.colorBorderSecondary}`,
+                }}
+                data-test-id={组件测试标识.视图编辑页面.配置栏目.下部}
+              >
+                toolPanel-bottom
+              </div>
             </div>
           </div>
+          <WidgetDrawer widgetOpenBtnRef={widgetOpenBtnRef} ref={drawerRef} />
         </div>
-        <WidgetDrawer widgetOpenBtnRef={widgetOpenBtnRef} ref={drawerRef} />
-      </div>
-      <div
-        style={{
-          borderTop: `1px solid ${token.colorBorderSecondary}`,
-          backgroundColor: token.colorBgContainer,
-          padding: layoutPadding(token),
-          zIndex: token.zIndexPopupBase + 1,
-        }}
-        data-test-id={组件测试标识.视图编辑页面.底部}
-      >
-        <Button
-          ref={widgetOpenBtnRef}
-          size="small"
-          type="text"
-          onClick={() => {
-            drawerRef.current?.switch();
+        <div
+          style={{
+            borderTop: `1px solid ${token.colorBorderSecondary}`,
+            backgroundColor: token.colorBgContainer,
+            padding: layoutPadding(token),
+            zIndex: token.zIndexPopupBase + 1,
           }}
+          data-test-id={组件测试标识.视图编辑页面.底部}
         >
-          部件库
-        </Button>
+          <Button
+            ref={widgetOpenBtnRef}
+            size="small"
+            type="text"
+            onClick={() => {
+              drawerRef.current?.switch();
+            }}
+          >
+            部件库
+          </Button>
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
